6912ae7cb3b6cd84226204dea60c72cfc5a5cb58
Sarika Pasumarthy <sarikakutty@gmail.com>
2021-01-25 16:55:03 -0800

    quiz

---
+++ .idea/quiz.py
@@ -0,0 +1,25 @@
+def quizme():
+    carscore = 0
+    artscore = 0
+    booksscore = 0
+    moviesscore = 0
+    questions = 0
+    q1 = ["pink", "red", "white", "blue"]
+    q2 = ["dog", "cat", "shark", "bird"]
+    q3 = ["tacos", "spring rolls", "pasta", "pizza"]
+    q4 = ["math", "english", "art", "music"]
+    q5 = ["monday", "thursday", "wednesday", "friday"]
+    print("what's your favorite color")
+    print(q1)
+    print("What's your favorite animal")
+    print(q2)
+    print("what's your favorite food")
+    print(q3)
+    print("what's your favorite school subject")
+    print(q4)
+    print("what's your favorite day of the week")
+    print(q5)
+
+quizme()
+
+
