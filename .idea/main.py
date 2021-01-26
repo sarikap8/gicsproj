9f22feacc78887978ddb1482939436d1ffc2d842
sarikap8 <51844103+sarikap8@users.noreply.github.com>
2021-01-25 18:47:26 -0800

    comments

--- .idea/main.py
+++ .idea/main.py
@@ -1,17 +1,21 @@
 import os
 import requests
-
+#modules needed to webscrape
 url = "https://trefle.io/api/v1/plants?token=5-M6tX9TBt0XVNIgKY7d1V26zFEPH8Vm-WoGmZQfaMI"
+#api url
 
 payload = {}
+#blank payload originally 
 
 response = requests.get(url, params=payload)
 
 responseJsonObj = response.json()
 dataList = responseJsonObj.get('data')
+#create a data obj
 
 recordedList1 = []
 for data in dataList:
+    #record information from webpage on dictionary
     common_name = data['common_name']
     scientific_name = data['scientific_name']
     year = data['year']
