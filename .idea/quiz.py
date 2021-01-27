99dd162e56662acbb2b447aaafbfd073e27cbeea
ryazhisai <73305760+ryazhisai@users.noreply.github.com>
2021-01-26 16:34:26 -0800

    fixed sarika's grammar

--- .idea/quiz.py
+++ .idea/quiz.py
@@ -5,7 +5,7 @@ import operator
 flower_list= ['Nelumbo nucifera','rose','Bellis perennis', 'Helianthus']
 
 flower_dict={}
-print("analyzing current flowers, then your quiz will begin! Sit tight!")
+print("Analyzing current flowers, then your quiz will begin! Sit tight!")
 for flower in flower_list:
     # Specify the title of the Wikipedia page
     wiki = wikipedia.page(flower)
